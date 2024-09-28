fe44c4589168b1f7200544ae6b535776f1a2f669
Cristian <cristian.jimenezsa@hotmail.com>
2024-09-27 23:07:38 -0500

    fixed contact path

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,6 +2,7 @@ import { defaultLang, showDefaultLang } from './ui';
 
 export function useTranslatedPath(lang: string) {
   return function translatePath(path: string, loc: string = lang) {
+    if (path.includes('mailto:')) return path;
     return !showDefaultLang && loc === defaultLang ? path : `/${loc}${path}`;
   };
 }
